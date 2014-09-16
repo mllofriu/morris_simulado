918506bb9d4924107d651913e56698c36fab2146
Martin Llofriu <mllofriu@gmail.com>
2014-09-16 17:11:41 +0000

    Changed infogatherer to work just with landmarks (no lines)

--- src/morris_simulation/nsl/infogatherer.py
+++ src/morris_simulation/nsl/infogatherer.py
@@ -1,4 +1,4 @@
-    #!/usr/bin/env python  
+#!/usr/bin/env python  
 '''
 Created on May 22, 2013
 
@@ -10,12 +10,12 @@ import roslib; roslib.load_manifest('morris_simulation')
 import rospy
 
 
-from affordances import Affordances
-from morris_simulation.perception import AffordanceCalc
-from MarkerCalc import MarkerCalc
+#from affordances import Affordances
+#from morris_simulation.perception import AffordanceCalc
+#from MarkerCalc import MarkerCalc
 from visualization_msgs.msg import  Marker
-from morris_simulation.msg import Lines
-from moveJoint import JointMover
+#from morris_simulation.msg import Lines
+#from moveJoint import JointMover
 from naoqi import ALProxy
 import message_filters
 
@@ -28,12 +28,12 @@ class InfoGatherer(object):
     def __init__(self):
         # Listen to visualization_markers for markers
         sub = message_filters.Subscriber("/visualization_marker", Marker)
-        self.markersCache = message_filters.Cache(sub, 200)
-        
-        self.mover = JointMover()
+        self.markersCache = message_filters.Cache(sub, 20)
+	rospy.sleep(5.)        
+#        self.mover = JointMover()
         
         # Fix pitch
-        self.mover.move("HeadPitch", [.7], .5)
+#        self.mover.move("HeadPitch", [.7], .5)
         rospy.loginfo( "Gatherer initiated")
         
         
@@ -41,13 +41,8 @@ class InfoGatherer(object):
         
         markers = []
         # Collect messages away from movement
-        start = rospy.Time.now()
-        for i in range(5):
-            self.linesCache.waitForTimedMessage(start)
-            # Cant wait for marker Messages as are published only on discovery
-            #self.markersCache.waitForTimedMessage(start)
-        lines += self.linesCache.getInterval(start, rospy.Time.now())
-        markers += self.markersCache.getInterval(start, rospy.Time.now())
+	now = rospy.Time.now()
+        markers += self.markersCache.query(now - rospy.Duration(10), now)
 
         print "Markers", markers
         marks = self.avgMarkers(markers)
@@ -69,9 +64,9 @@ class InfoGatherer(object):
                 x += ms.x
                 y += ms.y
                 z += ms.z
-            x /= markerSamples[k].length
-            y /= markerSamples[k].length
-            z /= markerSamples[k].length
+            x /= len(markerSamples[k])
+            y /= len(markerSamples[k])
+            z /= len(markerSamples[k])
             averagedMarkers += [(k,x,y,z)]
         
         return averagedMarkers
@@ -79,15 +74,15 @@ class InfoGatherer(object):
 if __name__ == "__main__":
     rospy.init_node('infoGatherer')
     
-    try:
-        motionProxy = ALProxy("ALMotion", "127.0.0.1", 9559)
-        motionProxy.wakeUp()
-    except Exception, e:
-        print "Could not create proxy to ALMotion"
-        print "Error was: ", e
+#    try:
+#        motionProxy = ALProxy("ALMotion", "127.0.0.1", 9559)
+#        motionProxy.wakeUp()
+#    except Exception, e:
+#        print "Could not create proxy to ALMotion"
+#        print "Error was: ", e
     
     
-    InfoGatherer().gather()
+    print InfoGatherer().gather()
     
     
         
