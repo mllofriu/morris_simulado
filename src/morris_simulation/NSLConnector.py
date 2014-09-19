9b2310d8f7ec43975c8185486a8939a14993cc3b
Martin Llofriu <mllofriu@gmail.com>
2014-09-19 21:19:42 +0000

    NSL connector debugging

--- src/morris_simulation/NSLConnector.py
+++ src/morris_simulation/NSLConnector.py
@@ -11,6 +11,7 @@ import rospy
 import socket
 from morris_simulation.nsl import InfoGatherer
 from morris_simulation.protobuf import connector_pb2 as proto
+import tf 
 
 from naoqi import ALProxy
 
@@ -21,20 +22,21 @@ class NSLConnector(object):
     classdocs
     '''
 
-    pilot = 0
-    
     def __init__(self):
         rospy.init_node('NSLConnector')
+
+        self.tf_listener = tf.TransformListener()
         
         # Flag that determines the validity of information gathered 
         self.validInformation = False
         self.markers = None
         self.afforances = None
         
-	self.infoGatherer = InfoGatherer();
+        self.infoGatherer = InfoGatherer();
         
         # Open the socket 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
+        s.settimeout(None)
         s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1) # Send every packet individually
 #        host = socket.gethostname() # Get local machine name
 #        print host
@@ -46,72 +48,92 @@ class NSLConnector(object):
 #        while True:
         self.con, addr = s.accept()     # Establish connection with client.
 #        self.processConnection()
-	try:
-	        self.motionProxy = ALProxy("ALMotion", '127.0.0.1', 9559)
-	except Exception, e:
-		print "Could not create proxy to ALMotion"
-		print "Error was: ", e
-	try:
-	        self.postureProxy = ALProxy("ALRobotPosture", '127.0.0.1', 9559)
-    	except Exception, e:
-        	print "Could not create proxy to ALRobotPosture"
-        	print "Error was: ", e
-	
+        try:
+            self.motionProxy = ALProxy("ALMotion", '127.0.0.1', 9559)
+        except Exception, e:
+            print "Could not create proxy to ALMotion"
+            print "Error was: ", e
+        try:
+            self.postureProxy = ALProxy("ALRobotPosture", '127.0.0.1', 9559)
+        except Exception, e:
+            print "Could not create proxy to ALRobotPosture"
+            print "Error was: ", e
+    
 
     def doAction(self, angle):
         print "Command",angle
         if angle == 0:
-            self.pilot.forwardMeters(.4)
+            x = .4
+            y = 0
+            t = 0
+            f = .5
         else:
-            self.pilot.rotateRad(angle*pi/180)
-        rospy.sleep(3)
+            x = 0
+            y = 0
+            t = angle/2
+            f = .5 
+        self.motionProxy.setWalkTargetVelocity(x, y, t, f)  
+        rospy.sleep(2)
         self.validInformation = False
         
         # Send Ok msg
-        okMsg = rp.Response()
+        okMsg = proto.Response()
         okMsg.ok = True;
-        self.con.sendall(okMsg.SerializeToString())
+        self.sendMsg(okMsg)
         
     def startRobot(self):
         print "Start Robot"
 
-	# Send NAO to Pose Init
-	self.motionProxy.setSmartStiffnessEnabled(True)
-    	self.postureProxy.goToPosture("StandInit", 0.5)	
+        # Send NAO to Pose Init
+        self.motionProxy.setSmartStiffnessEnabled(True)
+        self.postureProxy.goToPosture("StandInit", 0.5) 
 
+	names      = "HeadPitch"
+    	angleLists = .3
+    	timeLists  = .5
+    	isAbsolute = True
+    	self.motionProxy.angleInterpolation(names, angleLists, timeLists, isAbsolute)
         # Send Ok msg
         okMsg = proto.Response()
         okMsg.ok = True;
-        serial = okMsg.SerializeToString()
-        print "Largo", len(serial)
-        print "Msg", serial
-        self.con.sendall(serial)
-        print "ok sent"
-#                con.flush()
+        self.sendMsg(okMsg)
 
     def getInfo(self):
         if not self.validInformation:
             self.markers, self.affordances = self.infoGatherer.gather()
-	    print self.markers, self.affordances
-            self.validInformation = True
-	                
-	resp = proto.Response()
-	resp.ok = True
-	for b in self.affordances:
-		resp.affs.aff.append(b)
-	for lm in self.markers:
-		lm = proto.Landmark()
-		lm.id = lm[0]
-		lm.x = lm[1]
-		lm.y = lm[2]
-		lm.z = lm[3]
-		resp.landmarks.append(lm)
-      
-	print "Sending info"  
-	data = resp.SerializeToString()
+            #self.validInformation = True
+
+        resp = proto.Response()
+        resp.ok = True
+        for b in self.affordances:
+            resp.affs.aff.append(b)
+    
+        for lm in self.markers:
+            try:
+                self.tf_listener.waitForTransform("/base_footprint","/M"  + str(lm[0] + 1), rospy.Time(), rospy.Duration(3))
+                (t,rot) = self.tf_listener.lookupTransform("/base_footprint","/M" + str(lm[0] + 1), rospy.Time(0))
+            
+                plm = resp.landmarks.add()
+                # lm = (id, x, y, z) - x, y and z are not used since the transform is published
+                plm.id = lm[0]
+                #self.tf_listener.waitForTransform("/base_footprint","/M"  + str(plm.id + 1), now, rospy.Duration(.3))
+		# Transform coordinates from robot to model: x is the same, z is y and y = 0 (height)
+                plm.x = t[0]
+                plm.y = 0 #t[1]
+                plm.z = -t[1] #t[2]
+		print "Marca", (t[0], -t[1]) 
+                #resp.landmarks.append(lm)
+            except:
+                rospy.loginfo("Exception while trying to lookup transform") 
+
+        self.sendMsg(resp)
+
+    def sendMsg(self, resp):
+        print "Sending info"  
+        data = resp.SerializeToString()
         self.con.sendall(encoder._VarintBytes(len(data)) + data)
-	print "Info sent"
-     
+        print "Info sent"
+
     def processConnection(self):
         cmd = proto.Command()
         text = self.con.recv(4096)
@@ -129,8 +151,8 @@ class NSLConnector(object):
             cmd = proto.Command()
             cmd.ParseFromString(text)
        
-    	self.postureProxy.goToPosture("Crouch", 0.5)	
-	self.motionProxy.rest()
+        self.postureProxy.goToPosture("Crouch", 0.5)    
+        self.motionProxy.rest()
 
         # Send Ok msg
         #okMsg = proto.Response()
