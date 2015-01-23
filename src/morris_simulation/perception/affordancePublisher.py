dd1cb09599d85a10a9cd85e69177e26387caa029
Martin Llofriu <mllofriu@gmail.com>
2015-01-23 15:32:21 -0500

    Wait one frame if no lines are received

--- src/morris_simulation/perception/affordancePublisher.py
+++ src/morris_simulation/perception/affordancePublisher.py
@@ -14,22 +14,32 @@ from rospy import Duration
 
 class AffordancePublisher(object):
     affCalc = AffordanceCalc()
+    waitingNonEmptyLines = False
 
     def __init__(self):
         self.sub = rospy.Subscriber("/lines", PolygonsStamped, self.publishAffordances)
         self.pub = rospy.Publisher("/affordances", Affordances, queue_size=10)
         rospy.loginfo( "Affordance Publisher initiated")
+        
+        
     
     def publishAffordances(self, linesMsg):
+        # In case of receiving no lines, wait for next message to confirm 
+        if linesMsg.polygons == None and not self.waitingNonEmptyLines:
+            self.waitingNonEmptyLines = True;
+            return
+            
         affMsg = Affordances()
         affMsg.header.stamp = linesMsg.header.stamp
         
         if linesMsg.polygons != None:
             affMsg.affordances = self.affCalc.calcAffordances(linesMsg.polygons)
         else:
-            affMsg.affordances = self.affCalc.calcAffordances([])
+            affMsg.affordances = []
         
         self.pub.publish(affMsg);
+        
+        self.waitingNonEmptyLines = False
         
            
         
